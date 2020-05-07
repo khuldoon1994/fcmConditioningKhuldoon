87b15632ed3570fd50a5923128dd0a65d7b54b8a
Lars Radtke <lars.radtke@tuhh.de>
2020-05-07 09:54:31 +0200

    Added body load to truss 2d.

--- core/elementOperations/systemMatricesCreators/truss2dDynamicSystemMatricesCreator.m
+++ core/elementOperations/systemMatricesCreators/truss2dDynamicSystemMatricesCreator.m
@@ -24,9 +24,33 @@ function [ Me, Ke, Fe ] = truss2dDynamicSystemMatricesCreator(problem, elementIn
     s2 = s*s;
     c2 = c*c;
     
-    % single dof "matrices"
+    % element matrices
     Ke = E*A/L*[c2 sc -c2 -sc; sc s2 -sc -s2; -c2 -sc c2 sc; -sc -s2 sc s2];
     Me = rho*A*L/6*[2*c2 2*sc c2 sc; 2*sc 2*s2 sc s2; c2 sc 2*c2 2*sc; sc s2 2*sc 2*s2];
+        
+    % load vector
     Fe = zeros(4,1);
-    
+    if numel(problem.elementLoads{elementIndex})>=1
+         % create quadrature points
+        [ points, weights ] = quadraturePointGetter(problem, elementIndex);
+        nPoints = numel(weights);
+
+        % loop over quadrature points
+        for i=1:nPoints
+
+            % copy the local coordinates of this quadrature point
+            localCoordinates = points(:,i);
+
+            % shape functions and mapping evaluation
+            shapeFunctions = eoEvaluateShapeFunctions(problem, elementIndex, localCoordinates);
+             jacobian = eoEvaluateJacobian(problem,elementIndex,localCoordinates);
+            detJ = det(jacobian);
+
+            % add load vector integrand
+            N = moComposeInterpolationMatrix(problem.dimension,shapeFunctions);
+            b = eoEvaluateTotalLoad(problem, elementIndex, localCoordinates);
+            Fe = Fe + N'*b * weights(i) * detJ;
+
+        end
+    end
 end
