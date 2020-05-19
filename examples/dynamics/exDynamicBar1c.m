cc33b84ce861b0aabc6690aabf6f336a695000b2
gorji <mahan.gorji@tuhh.de>
2020-05-19 14:52:21 +0200

    adds dynamic example

---
+++ examples/dynamics/exDynamicBar1c.m
@@ -0,0 +1,119 @@
+% In SiHoFemLab, the problem definition is stored in a structure.
+% In this script, the following problem will be defined:
+%
+%                        f(x)
+%   /|---> ---> ---> ---> ---> ---> ---> --->
+%   /|=======================================
+%   /|          rho,E,A,L
+%
+% A bar, characterized by its density rho, Youngs modulus E, area A and
+% length L is loaded by a distributed force (one-dimensional "body-force").
+%
+% This elastodynamic problem will be analyzed using
+% Central Difference Method
+
+%% clear variables, close figures
+clear all;
+close all;
+clc;
+warning('off', 'MATLAB:nearlySingularMatrix'); % get with [a, MSGID] = lastwarn();
+
+%% problem definition
+problem.name = 'dynamicBar1D (Central Difference Method)';
+problem.dimension = 1;
+
+% parameter
+rho = 1.0;
+E = 1.0;
+A = 1.0;
+L = 1.0;
+f = @(x)( x/L );
+
+% damping parameter
+massCoeff = 1.0;
+stiffCoeff = 0.0;
+
+p = 1;
+n = 2;
+
+tStart = 0;
+tStop = 10;
+nTimeSteps = 401;
+
+problem = poCreateDynamicBarProblem(E, A, rho, L, p, n, f, ...
+                                    tStart, tStop, nTimeSteps, ...
+                                    massCoeff, stiffCoeff);
+
+indexOfLastNode = n+1;
+
+
+%% dynamic analysis
+displacementAtLastNode = zeros(problem.dynamics.nTimeSteps, 1);
+
+% create system matrices
+[ allMe, allDe, allKe, allFe, allLe ] = goCreateDynamicElementMatrices( problem );
+
+% assemble
+M = goAssembleMatrix(allMe, allLe);
+D = goAssembleMatrix(allDe, allLe);
+K = goAssembleMatrix(allKe, allLe);
+F = goAssembleVector(allFe, allLe);
+
+% set initial displacement and velocity
+[ nTotalDof ] = goNumberOfDof(problem);
+U0Dynamic = zeros(nTotalDof, 1);
+V0Dynamic = zeros(nTotalDof, 1);
+
+% compute initial acceleration
+[ A0Dynamic ] = goComputeInitialAcceleration(problem, M, D, K, F, U0Dynamic, V0Dynamic);
+
+% initialize values
+[ UOldDynamic, UDynamic ] = cdmInitialize(problem, U0Dynamic, V0Dynamic, A0Dynamic);
+
+% create effective system matrices
+[ KEff ] = cdmEffectiveSystemStiffnessMatrix(problem, M, D, K);
+
+for timeStep = 1 : problem.dynamics.nTimeSteps
+    
+    % extract necessary quantities from solution
+    displacementAtLastNode(timeStep) = UDynamic(3);
+    
+    % calculate effective force vector
+    [ FEff ] = cdmEffectiveSystemForceVector(problem, M, D, K, F, UDynamic, UOldDynamic);
+    
+    FEff(3) = FEff(3) + 1;
+    
+    % solve linear system of equations (UNewDynamic = KEff \ FEff)
+    UNewDynamic = moSolveSparseSystem( KEff, FEff );
+    
+    % calculate velocities and accelerations
+    [ VDynamic, ADynamic ] = cdmVelocityAcceleration(problem, UNewDynamic, UDynamic, UOldDynamic);
+    
+    % update kinematic quantities
+    [ UDynamic, UOldDynamic ] = cdmUpdateKinematics(UNewDynamic, UDynamic);
+    
+end
+
+% disassemble
+[ allUe ] = goDisassembleVector( UDynamic, allLe );
+
+% check for stability
+poCheckDynamicStabilityCDM(problem, M, K);
+
+
+%% post processing
+timeVector = goGetTimeVector(problem);
+
+% steady state value (of displacement at last node)
+uSteadyState = 16/48;
+
+% plotting necessary quantities over time
+figure(1);
+plot(timeVector, displacementAtLastNode/uSteadyState, 'LineWidth', 1.6);
+hold on;
+grid on;
+plot([timeVector(1), timeVector(end)], [1 1], 'k:', 'LineWidth', 1.6);
+
+title([problem.dynamics.timeIntegration, ' Method: u_{norm}(t, x = L)']);
+xlabel('Time [sec]');
+ylabel('normalized Displacement [-]');
