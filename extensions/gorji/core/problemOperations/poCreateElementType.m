a4dd3af207dc271bc0582d31e716c1eb78ac0268
gorji <mahan.gorji@tuhh.de>
2020-06-15 12:47:23 +0200

    beam test.

---
+++ extensions/gorji/core/problemOperations/poCreateElementType.m
@@ -0,0 +1,39 @@
+function [ newType ] = poCreateElementType(  typeName, typeData )
+% poCreateElementType Adds a standard (or dynamic, FCM, etc.) element type by name.
+%   
+%   newType = poCreateElementType(typeName, typeData) craetes a new
+%   element type with name typeName according to the parameters given in
+%   typeData.
+%
+%   typeName must be a character array representing a known type name.
+%   typeData must be a structure array with fields as expected by the type.
+%
+%   See also poCreateElementTypeStandardLine1d,
+%   poCreateElementTypeStandardLine2d, poCreateElementTypeStandardQuad2d,
+
+    names = { 'STANDARD_LINE_1D', ...
+              'STANDARD_LINE_2D', ...
+              'STANDARD_QUAD_2D', ...
+              'FCM_LINE_1D', ...
+              'STANDARD_POINT_2D', ...
+              'STANDARD_TRUSS_2D', ...
+              'STANDARD_BEAM_1D' };
+    creators = { @poCreateElementTypeStandardLine1d, ...
+                 @poCreateElementTypeStandardLine2d, ...
+                 @poCreateElementTypeStandardQuad2d, ...
+                 @poCreateElementTypeFCMLine1d, ...
+                 @poCreateElementTypeStandardPoint2d, ...
+                 @poCreateElementTypeStandardTruss2d, ...
+                 @poCreateElementTypeStandardBeam1d };
+            
+    iType = find(strcmp(names,typeName));
+             
+    if isscalar(iType)
+        newType = creators{iType}(typeData);
+    else
+        disp(['ERROR! Element type name ', typeName, ' is unknown. Choose one of ']);
+        disp(names);
+    end
+
+end
+
