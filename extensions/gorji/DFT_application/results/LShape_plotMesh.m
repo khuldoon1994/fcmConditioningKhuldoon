e69136728145cbbabeea616b62c2dfb835266f3a
gorji <mahan.gorji@tuhh.de>
2020-07-13 03:29:51 +0200

    tiny change.

--- extensions/gorji/DFT_application/results/LShape_plotMesh.m
+++ extensions/gorji/DFT_application/results/LShape_plotMesh.m
@@ -7,6 +7,10 @@ scaleLoad = 5e-4;
 
 % plot mesh and boundary conditions
 goPlotMesh(problem,1);
+
+% save figure
+saveas(gcf,'results/results_LShape_mesh.jpg');
+
 goPlotLoads(problem,1,scaleLoad);
 goPlotPenalties(problem,1);
 title('Problem setup');
