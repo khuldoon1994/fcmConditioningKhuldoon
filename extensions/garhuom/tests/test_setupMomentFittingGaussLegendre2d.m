564fa25f3ec530b9b18f20403889a554da7b0f6f
Garhuom <wadhah.garhuom@tuhh.de>
2020-10-05 17:21:46 +0200

    implement the moment fitting quadrature

---
+++ extensions/garhuom/tests/test_setupMomentFittingGaussLegendre2d.m
@@ -0,0 +1,52 @@
+
+%%
+
+clear
+clc
+close
+
+%%
+pA = 2;
+
+quadraturePointGetterData.levelSetFunction = @(X) -( (X(1,:) - 1).^2 + (X(2,:) - 1).^2 - 0.75^2 );
+quadraturePointGetterData.depth = 9;
+quadraturePointGetterData.gaussOrder = 2*pA+1;
+quadraturePointGetterData.alphaFCM = 1.0e-7;
+
+%%
+problem.name='Test: Setup moment fitting integration 2d';
+problem.dimension = 2;
+
+problem.nodes=[0 2 0 2 ; 0 0 2 2];
+
+elementType1 = poCreateElementType( 'STANDARD_QUAD_2D', struct( 'gaussOrder', 2*pA+1, 'physics', 'PLANE_STRAIN', 'youngsModulus', 1, 'poissonRatio', 0.3 ) );
+elementType1.quadraturePointGetterData = quadraturePointGetterData;
+
+problem.elementTypes = { elementType1 };
+
+problem.elementNodeIndices = { [ 1 2 3 4 ] };
+problem.elementTopologies = [ 2 ];
+problem.elementTypeIndices = [ 1 ];
+
+%%
+
+%problem.elementQuadratures = {eoSetupAdaptiveGaussLegendre2d( problem, 1 )};
+problem.elementQuadratures = {setupMomentFittingGaussLegendre2d( problem, 1 )};
+
+ng = length(problem.elementQuadratures{1}.weights)
+
+%%
+
+Aq = sum( problem.elementQuadratures{1}.weights )
+Aex = 4 - pi*0.75^2
+
+e_r = abs( (Aq - Aex) / Aex )
+
+%% plot adaptive gauss legendre 2d
+
+plotAdaptiveGaussLegendre2d( problem, 1 )
+
+points = problem.elementQuadratures{1}.points;
+weights = problem.elementQuadratures{1}.weights;
+
+
